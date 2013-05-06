270b79cfd6fa987611564c4ae88cf82328654f79
Patrick Smith <patrick@nerdgeekhero.(none)>
2013-05-06 11:01:37 +0000

    updates

--- app.js
+++ app.js
@@ -165,12 +165,16 @@ update_wallets = function()
   console.log("Updating wallets");
 	btceTrade.getInfo(function(err,data) {
 		if (err) {
-			throw err;
+			console.log("Error updating wallet");
+			console.log(err);
 		}
-		wallet.usd = data.funds.usd;
-		wallet.ltc = data.funds.ltc;
-		wallet.btc = data.funds.btc;
-    console.log(wallet);
+		else
+		{
+			wallet.usd = data.funds.usd;
+			wallet.ltc = data.funds.ltc;
+			wallet.btc = data.funds.btc;
+    			console.log(wallet);
+		}
 	});
 }
 
@@ -228,6 +232,7 @@ update_tickers = function()
     
     btceTrade.orderList({active: 1},function(err,data){
       console.log("OrderList:");
+      console.log(data);
       if (greaseltc > cash && cash >= 1 && !data)
       {
         do_greaseltc({'ltc_usd': {'rate':ltc_usd.buy,'amount':floorFigure(cash / ltc_usd.buy,8)}, 'ltc_btc': {'rate':ltc_btc.sell,'amount': floorFigure(cash / ltc_usd.buy,8)}, 'btc_usd': {'rate':btc_usd.sell,'amount': floorFigure(floorFigure(cash / ltc_usd.buy,8) * ltc_btc.sell,8)} });
